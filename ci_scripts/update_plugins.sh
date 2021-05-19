e1c11267cd152d72205c2c11967c15cb06059729
Lucy Lu <61244607+hs-llu@users.noreply.github.com>
2021-05-19 15:42:51 -0400

    Update update_plugins.sh

--- ci_scripts/update_plugins.sh
+++ ci_scripts/update_plugins.sh
@@ -2,5 +2,5 @@
 prefix="#LAST_CACHED_"
 new_date=$(date '+%Y%m%d')
 sed_command="s/${prefix}[0-9]{8}/$prefix$new_date/g"
-sed -i '' -r $sed_command plugins.txt
+sed -i '' -e $sed_command plugins.txt
 sort -o plugins.txt{,}
